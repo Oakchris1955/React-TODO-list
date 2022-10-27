a2e318031567eaa873caaa2b801e81f6c4fc40aa
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2022-10-27 15:53:30 +0300

    Support for multiple lists added
    This took me a long time to make. Basically, instead of having 1 list, you can have multiple lists now. There are some stylish changes left, but everything is working as intented

--- App.tsx
+++ App.tsx
@@ -1,61 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect ,Component } from 'react';
-import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useEffect, Component } from 'react';
+import { Button, Modal, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 import BouncyCheckbox from "react-native-bouncy-checkbox";
+import ModalDropdown from 'react-native-modal-dropdown';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-interface DeserialisedList {
-	TODO_list: {
-		text: string,
-		isDone: boolean
-	}[]
+
+interface DeserialisedStorage {
+	SavedLists: {
+		[index: string]: {
+			text: string,
+			isDone: boolean
+		}[]
+	},
+	LastOpened: string
 }
 
 class App extends Component {
-	//list: {text: string, isDone: boolean}[] = [];
 	state: {
-		//list: {text: string, isDone: boolean}[],
-		TODO_list: TODO_Entry[]
+		CurrentList: Entry[],
+		modalStates: {
+			remove: boolean,
+			append: boolean
+		}
 	} = {
-		//list: [],
-		TODO_list: []
+		CurrentList: [],
+		modalStates: {
+			remove: false,
+			append: false
+		}
 	};
 
 	inputText?: string;
 	maxKey: number = 0;
 
-	
+	savedStorage: DeserialisedStorage = {
+		SavedLists: {},
+		LastOpened: "TODO"
+	};
+
+	appendModalInputText: string | null = null;
+
+
 	constructor(props: any) {
 		super(props);
 		this._readStorage()
-		.then((todo_list) => {
-			console.log(todo_list);
-			this.setState({TODO_list: todo_list.map(
-				(item) => {
-					console.log(`${item.text}: ${item.isDone}`);
-					return new TODO_Entry({
-						text: item.text,
+		.then((storagevar) => {
+			this.savedStorage = storagevar;
+			this.setState({
+				CurrentList: storagevar.SavedLists[storagevar.LastOpened].map(
+					(value) => 	
+					new Entry({
+						text: value.text,
 						parent: this,
-						isDone: item.isDone
+						isDone: value.isDone
 					})
-				})
+				)
 			});
 		})
 		.catch();
 	}
 
-
-	
 	async _readStorage() {
 		try {
-			const value = await AsyncStorage.getItem("TODO_list");
+			const value = await AsyncStorage.getItem("ListData");
 			if (value !== null) {
-				let DeserialisedValue: DeserialisedList = JSON.parse(value);
+				let DeserialisedValue: DeserialisedStorage = JSON.parse(value);
 				console.log(`Loaded ${DeserialisedValue}`);
 				console.log(DeserialisedValue);
-				return DeserialisedValue.TODO_list;
+				return DeserialisedValue;
 			} else {
-				return [] as DeserialisedList["TODO_list"]
+				return {
+					SavedLists: {
+						"TODO": []
+					},
+					LastOpened: "TODO"
+				} as DeserialisedStorage
 			}
 		} catch(err) {
 			throw err
@@ -63,62 +84,138 @@ class App extends Component {
 	}
 
 	componentDidUpdate() {
+		console.log("A component updated");
+		this.syncStorage();
 		this.saveStorage().catch();
+	}
+
+	syncStorage() {
+		this.savedStorage.SavedLists[this.savedStorage.LastOpened]
+		= this.state.CurrentList.map(
+			(item) => item.getInfo()
+		);
+		console.log("Synced storage");
 	}
 
 	async saveStorage() {
 		try {
-			let props_list = this.state.TODO_list.map(
-				(item) => item.getInfo()
-			);
-			let SerialisedList = JSON.stringify({TODO_list: props_list});
+			let SerialisedList = JSON.stringify(this.savedStorage);
 			console.log(`Saved ${SerialisedList}`);
-			await AsyncStorage.setItem("TODO_list", SerialisedList);
+			await AsyncStorage.setItem("ListData", SerialisedList);
 		} catch(err) {
 			throw err
 		}
 	}
 
+	newList(name: string) {
+		this.savedStorage.SavedLists[name] = [];
+		this.saveStorage();
+	}
+
+	removeList(name: string) {
+		delete this.savedStorage.SavedLists[name];
+		console.log(`Removed "${name}"`)
+		this.saveStorage();
+	}
+
+	changeList(name: string) {
+		this.savedStorage.LastOpened = name;
+		this.saveStorage();
+		this._readStorage()
+		.then((storagevar) => {
+			this.savedStorage = storagevar;
+			this.setState({
+				CurrentList: storagevar.SavedLists[storagevar.LastOpened].map(
+					(value) => 	
+					new Entry({
+						text: value.text,
+						parent: this,
+						isDone: value.isDone
+					})
+				)
+			});
+		})
+		.catch();
+	}
+
 	_add_to_list() {
 		if (typeof this.inputText !== "undefined") {
-			/*this.setState({list: this.state.list.concat(
-				{
-					text: this.inputText,
-					isDone: false
-				}
-			)});*/
-			this.setState({TODO_list: this.state.TODO_list.concat(
-				new TODO_Entry({
+			this.setState({CurrentList: this.state.CurrentList.concat(
+				new Entry({
 					text: this.inputText,
 					parent: this
 				})
 			)});
-			//console.log((<TODO_Entry text={this.inputText} isDone={false} key={this.maxKey} removeElement={() => {removeElement(this, nextListIndex)}}/>));
 		}
 	}
 	check_to_remove() {
-		/*let newList = this.state.list.filter((item, index) => {
-			item.key
-		});*/
-		let editedList = this.state.TODO_list.filter((item) => !(item.toBeRemoved));
-		if (!this.state.TODO_list.every((val, index) => val === editedList[index])) {
+		let editedList = this.state.CurrentList.filter((item) => !(item.toBeRemoved));
+		if (!this.state.CurrentList.every((val, index) => val === editedList[index])) {
 			this.setState({
-				TODO_list: editedList
+				CurrentList: editedList
 			});
 		}
 	}
 	render() {
-		/*const finalList = this.state.list.map((item, index) => 
-			<TODO_Entry text={item.text} isDone={item.isDone} key={index}/>
-		);*/
-		//console.log(finalList);
-		//this._check_to_remove();
-		const finalList: JSX.Element[] = this.state.TODO_list.map(
+		const finalList: JSX.Element[] = this.state.CurrentList.map(
 			(entry) => entry.render()
 		);
+
+		const finalListRemovableNames = Object.keys(this.savedStorage.SavedLists).map(
+			(key) => {
+				return (key !== this.savedStorage.LastOpened ? <CustomDisplay text={key} removable={true} parent={this} key={this.maxKey+=1}/> : null)
+			}
+		);
+
+		const finalListNames = Object.keys(this.savedStorage.SavedLists).map(
+			(key) => {
+				return <CustomDisplay text={key} removable={false} parent={this} key={this.maxKey+=1}/>
+			}
+		);
 		console.log("Re-rendered App");
+		console.log(this.state.modalStates);
+
 		return (
 			<View style={styles.container}>
+				<View style={styles.DropdownContainer}>
+					<Pressable style={{marginHorizontal: "5%", minWidth: "5%"}} onPress={() => this.setState({modalStates: {remove: true, append: false}})/*this.state.modalObject.changeStatus(ModalStatus.Add)*/}><Text>-</Text></Pressable>
+					<ModalDropdown onSelect={(_, selectedString) => {if (selectedString != this.savedStorage.LastOpened) this.changeList(selectedString)}} textStyle={{minWidth: 40, borderColor: "purple", borderWidth: 2, borderRadius: 20}} dropdownStyle={styles.DropdownStyle} defaultValue={this.savedStorage.LastOpened} options={Object.keys(this.savedStorage.SavedLists)}/>
+					<Pressable style={{marginHorizontal: "5%", minWidth: "5%"}} onPress={() => this.setState({modalStates: {remove: false, append: true}})}><Text>+</Text></Pressable>
+				</View>
+				
+				<Modal visible={this.state.modalStates.remove} animationType="fade" transparent={true}>
+					<View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+						<View style={{backgroundColor: "white", borderRadius: 20, padding: 20, justifyContent: "center", alignItems: "center"}}>
+							<Text>
+								Please select an element to remove:
+							</Text>
+							{finalListRemovableNames}
+							<Pressable style={{borderColor: "lightblue", backgroundColor: "lightblue", borderWidth: 4, borderRadius: 35, paddingHorizontal: 2}} onPress={() => this.setState({modalStates: {remove: false, append: false}})}>
+								<Text>Close</Text>
+							</Pressable>
+						</View>
+					</View>
+				</Modal>
+
+				<Modal visible={this.state.modalStates.append} animationType="fade" transparent={true}>
+					<View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+						<View style={{backgroundColor: "white", borderRadius: 20, padding: 20, justifyContent: "center", alignItems: "center"}}>
+							<View style={{flexDirection: "row", marginBottom: 5}}>
+								<View style={{alignItems: "center"}}>
+									<TextInput placeholder='New list name:' onChangeText={(text) => this.appendModalInputText = text === "" ? null : text}/>
+									<Pressable style={{borderColor: "lightgreen", backgroundColor: "lightgreen", borderWidth: 4, borderRadius: 35, paddingHorizontal: 2}} onPress={() => {if (this.appendModalInputText != null) this.newList(this.appendModalInputText)}}>
+										<Text>Create new list</Text>
+									</Pressable>
+								</View>
+								<View>{finalListNames}</View>
+							</View>
+							<Pressable style={{borderColor: "lightblue", backgroundColor: "lightblue", borderWidth: 4, borderRadius: 35, paddingHorizontal: 2}} onPress={() => this.setState({modalStates: {remove: false, append: false}})}>
+								<Text>Close</Text>
+							</Pressable>
+						</View>
+					</View>
+				</Modal>
+				
 				<View style={{justifyContent: "center", alignItems: "center", width:"100%"}}>
 					{finalList}
 				</View>
@@ -130,13 +227,56 @@ class App extends Component {
 	}
 }
 
-interface TODO_Entry_Props {
+interface CustomDisplayProps {
+	text: string,
+	removable: boolean,
+	parent: App
+}
+
+class CustomDisplay extends Component<CustomDisplayProps> {
+	parent: App;
+	text: string;
+	removable: boolean;
+
+	constructor(props: CustomDisplayProps) {
+		super(props);
+		this.parent = this.props.parent;
+		this.text = this.props.text;
+		this.removable =  this.props.removable;
+	}
+
+	styles = StyleSheet.create({
+		parentView: {
+			flexDirection:'row',
+			marginTop: 10
+		},
+		children: {
+			marginHorizontal: "5%",
+			borderColor: "lightgrey",
+			borderWidth: 2,
+			borderRadius: 15,
+			minWidth: 50
+		}
+	});
+
+	render() {
+		return (
+			<View style={this.styles.parentView}>
+				<Text style={this.styles.children}>{this.text}</Text>
+				{this.removable ? <Pressable onPress={() => {this.parent.removeList(this.text);}}><Icon name='close' size={20}/></Pressable> : null}
+			</View>
+		)
+	}
+}
+
+
+interface Entry_Props {
 	text: string
 	parent: App
 	isDone?: boolean
 }
 
-class TODO_Entry extends Component<TODO_Entry_Props> {
+class Entry extends Component<Entry_Props> {
 	done: boolean;
 	text: string;
 	parent: App;
@@ -162,7 +302,7 @@ class TODO_Entry extends Component<TODO_Entry_Props> {
 		}
 	});
 
-	constructor(props: TODO_Entry_Props) {
+	constructor(props: Entry_Props) {
 		super(props);
 		this.done = typeof props.isDone === "undefined" ? false : props.isDone;
 		this.text = props.text;
@@ -170,7 +310,7 @@ class TODO_Entry extends Component<TODO_Entry_Props> {
 		this.key = this.parent.maxKey += 1;
 	}
 
-	getInfo(): DeserialisedList["TODO_list"][0] {
+	getInfo(): DeserialisedStorage["SavedLists"][any][any] {
 		return {
 			text: this.text,
 			isDone: this.done
@@ -180,7 +320,7 @@ class TODO_Entry extends Component<TODO_Entry_Props> {
 	render() {
 		return (
 			<View style={{flexDirection: "row", margin: 2}} key={this.key}>
-				<BouncyCheckbox text={this.text} fillColor="black" textStyle={this.styles.BouncyText} style={this.styles.BouncyStyle} onPress={(isChecked) => {this.done = isChecked;this.parent.saveStorage()}} isChecked={this.done} />
+				<BouncyCheckbox text={this.text} fillColor="black" textStyle={this.styles.BouncyText} style={this.styles.BouncyStyle} onPress={(isChecked) => {this.done = isChecked;this.parent.syncStorage();this.parent.saveStorage();}} isChecked={this.done} />
 				<Pressable style={this.styles.pressableStyle} onPress={() => {this.toBeRemoved = true;this.parent.check_to_remove()}}>
 					<Text>
 						Remove
@@ -215,6 +355,18 @@ const styles = StyleSheet.create({
 	},
 	sameLine: {
 		flexDirection: 'row'
+	},
+	DropdownContainer: {
+		top: 0,
+		left: 0,
+		position: "absolute",
+		marginTop: "10%",
+		marginLeft: "10%",
+		flexDirection: "row"
+	},
+	DropdownStyle: {
+		height: "auto",
+		minWidth: "20%"
 	},
 	marginedContent: {
 		margin: 2.5
